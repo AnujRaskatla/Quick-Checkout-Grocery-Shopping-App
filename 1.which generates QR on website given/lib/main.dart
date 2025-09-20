2ba78558345883765e619bc9b9be8a0520469f09
AnujRaskatla <anuj.raskatla@gmail.com>
2025-09-20 19:15:31 +0530

    Add 1.which generates QR on website given

---
+++ 1.which generates QR on website given/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qr_code/generate_qr_code.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(primaryColor: Colors.black54, primarySwatch: Colors.brown),
+      home: const GenerateQRCode(),
+    );
+  }
+}

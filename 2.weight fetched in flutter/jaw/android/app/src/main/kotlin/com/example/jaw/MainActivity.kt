4cd0877a50b821d3046c3b27313e3acef2efddd2
AnujRaskatla <anuj.raskatla@gmail.com>
2025-09-20 19:15:39 +0530

    Keep only last folder GitRepo

--- 2.weight fetched in flutter/jaw/android/app/src/main/kotlin/com/example/jaw/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jaw
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- exit/android/app/src/main/kotlin/com/example/exit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.exit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

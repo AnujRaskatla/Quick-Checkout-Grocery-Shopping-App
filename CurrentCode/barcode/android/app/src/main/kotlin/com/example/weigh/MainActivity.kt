1d4aeac0dd6d518a53e7ba502ef0de3dcd82b6da
AnujRaskatla <anuj.raskatla@gmail.com>
2025-09-20 18:38:15 +0530

    Version: 24.Added received weight in viewlistpage

---
+++ CurrentCode/barcode/android/app/src/main/kotlin/com/example/weigh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weigh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- CurrentCode/wassup/android/app/src/main/kotlin/com/example/wassup/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wassup
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

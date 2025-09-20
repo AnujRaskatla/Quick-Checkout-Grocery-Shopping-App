c7bb0cfc392074f4525cad83ef719506f05496b5
AnujRaskatla <anuj.raskatla@gmail.com>
2025-09-20 18:39:20 +0530

    Version: 3.welcome page saying scanQr to take to another page

--- CurrentCode/welcome/lib/main.dart
+++ CurrentCode/welcome/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,30 +52,8 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class QRPage extends StatefulWidget {
+class QRPage extends StatelessWidget {
   const QRPage({super.key});
-
-  @override
-  QRPageState createState() => QRPageState();
-}
-
-class QRPageState extends State<QRPage> {
-  String _scannedValue = "ScannedValue";
-
-  Future<void> _scanQR() async {
-    String scanResult = await FlutterBarcodeScanner.scanBarcode(
-      "#ff6666", // Color for the scan button
-      "Cancel", // Text for the cancel button
-      false, // Show flash icon
-      ScanMode.QR, // Specify the scan mode (QR code in this case)
-    );
-
-    if (!mounted || scanResult == '-1') return;
-
-    setState(() {
-      _scannedValue = scanResult;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,40 +61,18 @@ class QRPageState extends State<QRPage> {
       appBar: AppBar(
         title: const Text('QR Page'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.blue,
-              child: const Center(
-                child: Text(
-                  'Scan QR Code',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+      body: Container(
+        color: Colors.blue,
+        child: const Center(
+          child: Text(
+            'QR Scan Page',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              _scannedValue,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: _scanQR,
-            child: const Text('Start Scanning'),
-          ),
-        ],
+        ),
       ),
     );
   }
